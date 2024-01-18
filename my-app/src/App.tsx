099f407bddd87d9bda3f86dfaf3374794e0d5c37
Hassaan Ehmed <hassutechap@gmail.com>
2024-01-18 16:36:35 +0500

    Redux Learing Done!

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,23 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useAppDispatch, useAppSelector } from './redux/hooks';
+import {increment, decrement} from './redux/slices/counter'
 
 function App() {
+
+// For Reading Value from Redux Store🏪
+
+// goto my store and grab reducer obj 
+//where (I define all reducers ) so {reducer.count}
+// reducer.count = reducer{ count: counterSlice }    
+     
+const count = useAppSelector(state =>  state.count)
+
+//to send values in our store
+
+const dispatch = useAppDispatch()
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <h1>Counter: {count}</h1>
+
+        <button
+        onClick={()=>dispatch(increment())}
+        
+        
+        >Increment</button>
+        <button
+        onClick={()=>dispatch(decrement())}
+        
+        
+        >decrement</button>
+        
       </header>
     </div>
   );
